c0c18e50191bd76ff5da7deb02cffd13f339ab66
Gaurav Jain <gaurav.jain@harness.io>
2022-01-18 21:38:30 +0530

    fix: [DEL-3055]: Making watcher upgrade executor code block synchronised.

--- 260-delegate/src/main/java/io/harness/delegate/app/modules/DelegateModule.java
+++ 260-delegate/src/main/java/io/harness/delegate/app/modules/DelegateModule.java
@@ -617,11 +617,11 @@ import lombok.RequiredArgsConstructor;
 public class DelegateModule extends AbstractModule {
   private final DelegateConfiguration configuration;
 
-    /*
-     * Creates and return ScheduledExecutorService object, which can be used for health monitoring purpose
-     * Currently this executor has been used for sending heartbeat to manager and watcher. This is also being
-     * used to send KeepAlive packet to manager.
-     */
+  /*
+   * Creates and return ScheduledExecutorService object, which can be used for health monitoring purpose
+   * Currently this executor has been used for sending heartbeat to manager and watcher. This is also being
+   * used to send KeepAlive packet to manager.
+   */
   @Provides
   @Singleton
   @Named("healthMonitorExecutor")
@@ -632,11 +632,11 @@ public class DelegateModule extends AbstractModule {
     return healthMonitorExecutor;
   }
 
-    /*
-     * Creates and return ScheduledExecutorService object, which can be used for monitoring watcher.
-     * Note that, this will only be used for monitoring, any action taken will be than executed by some
-     * another threadpool.
-     */
+  /*
+   * Creates and return ScheduledExecutorService object, which can be used for monitoring watcher.
+   * Note that, this will only be used for monitoring, any action taken will be than executed by some
+   * another threadpool.
+   */
   @Provides
   @Singleton
   @Named("watcherMonitorExecutor")
@@ -647,9 +647,9 @@ public class DelegateModule extends AbstractModule {
     return watcherMonitorExecutor;
   }
 
-    /*
-     * Creates and return ScheduledExecutorService object, which can be used for scheduling self upgrade check.
-     */
+  /*
+   * Creates and return ScheduledExecutorService object, which can be used for scheduling self upgrade check.
+   */
   @Provides
   @Singleton
   @Named("upgradeExecutor")
@@ -660,10 +660,10 @@ public class DelegateModule extends AbstractModule {
     return upgradeExecutor;
   }
 
-    /*
-     * Creates and return ScheduledExecutorService object, which can be used for reading message from
-     * watcher and take appropriate action.
-     */
+  /*
+   * Creates and return ScheduledExecutorService object, which can be used for reading message from
+   * watcher and take appropriate action.
+   */
   @Provides
   @Singleton
   @Named("inputExecutor")
@@ -674,10 +674,10 @@ public class DelegateModule extends AbstractModule {
     return inputExecutor;
   }
 
-    /*
-     * Creates and return ScheduledExecutorService object, which can be used for tracking delegate profile
-     * and execute it in case of any changes.
-     */
+  /*
+   * Creates and return ScheduledExecutorService object, which can be used for tracking delegate profile
+   * and execute it in case of any changes.
+   */
   @Provides
   @Singleton
   @Named("profileExecutor")
@@ -688,9 +688,9 @@ public class DelegateModule extends AbstractModule {
     return profileExecutor;
   }
 
-    /*
-     * Creates and return ScheduledExecutorService object, which can be used for fetching task in case of polling.
-     */
+  /*
+   * Creates and return ScheduledExecutorService object, which can be used for fetching task in case of polling.
+   */
   @Provides
   @Singleton
   @Named("rescheduleExecutor")
@@ -701,10 +701,20 @@ public class DelegateModule extends AbstractModule {
     return rescheduleExecutor;
   }
 
-    /*
-     * Creates and return ExecutorService object, which can be used for performing low priority activities.
-     * Currently, this is being used for performing graceful stop.
-     */
+  @Provides
+  @Singleton
+  @Named("verificationExecutor")
+  public ScheduledExecutorService verificationExecutor() {
+    ScheduledExecutorService verificationExecutor = new ScheduledThreadPoolExecutor(
+        2, new ThreadFactoryBuilder().setNameFormat("verification-%d").setPriority(Thread.NORM_PRIORITY).build());
+    Runtime.getRuntime().addShutdownHook(new Thread(verificationExecutor::shutdownNow));
+    return verificationExecutor;
+  }
+
+  /*
+   * Creates and return ExecutorService object, which can be used for performing low priority activities.
+   * Currently, this is being used for performing graceful stop.
+   */
   @Provides
   @Singleton
   @Named("backgroundExecutor")
@@ -715,9 +725,9 @@ public class DelegateModule extends AbstractModule {
     return backgroundExecutor;
   }
 
-    /*
-     * Creates and return ExecutorService object, which can be used for performing watcher upgrade.
-     */
+  /*
+   * Creates and return ExecutorService object, which can be used for performing watcher upgrade.
+   */
   @Provides
   @Singleton
   @Named("watcherUpgradeExecutor")
